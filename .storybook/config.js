f24f25083e49a74238f5504fa98b9aa6ac870abf
jungdj <jung_dj@kaist.ac.kr>
2019-04-25 04:09:29 +0900

    feat(Storybook): Add addons and configs

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,47 @@
-import { configure } from '@storybook/react';
+import { addDecorator, configure, addParameters } from '@storybook/react'
+import { withNotes } from '@storybook/addon-notes'
+import '@storybook/addon-console'
+import { withInfo } from "@storybook/addon-info"
+import { withKnobs } from '@storybook/addon-knobs';
+import { withConsole } from '@storybook/addon-console'
+
+import requireContext from 'require-context.macro'
+
+import React from "react"
+
+// import { Provider } from "react-redux"
+// import { BrowserRouter as Router } from "react-router-dom"
+// import { ThemeProvider } from "styled-components"
+
+// import store from "../src/store"
+// import theme from "../src/theme"
+
+// import '../src/index.scss'
+
+// const Decorator = (storyFn) => (
+// 	<Provider store={store}>
+// 		<Router>
+// 			<ThemeProvider theme={theme}>
+// 				{storyFn()}
+// 			</ThemeProvider>
+// 		</Router>
+// 	</Provider>
+// )
+
+// addDecorator(Decorator)
+addDecorator(withNotes)
+addDecorator((storyFn, context) => withConsole()(storyFn)(context))
+//addDecorator(withInfo)
+addDecorator(withKnobs)
+
+addParameters({
+	chromatic: { viewports: [1320, 992, 768, 576] },
+})
+
+const req = requireContext('../src/components', true, /\.stories\.js$/)
 
 function loadStories() {
-  require('../src/stories');
+	req.keys().forEach(filename => req(filename))
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
