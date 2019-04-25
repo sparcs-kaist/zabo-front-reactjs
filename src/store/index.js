26aaa100ddd188c84237b353810cdbcb1df48747
jin <jin@coinone.co.id>
2019-04-25 13:14:20 +0900

    feat(redux): Connect redux store with App using redux provider
    Use redux-pender for ajax store updates

---
+++ src/store/index.js
@@ -0,0 +1,32 @@
+import { applyMiddleware, compose, createStore } from "redux"
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly"
+import penderMiddleware from "redux-pender"
+
+import rootReducer from "./reducers"
+
+const composeEnhancers =
+	process.env.NODE_ENV === "production"
+		? compose
+		: composeWithDevTools({
+				actionBlacklist: [
+					"@@redux-pender/SUCCESS",
+					"@@redux-pender/FAILURE",
+					"@@redux-pender/PENDING",
+				],
+				maxAge: 1000,
+		  })
+
+const store = createStore(
+	rootReducer,
+	{}, // initial state
+	composeEnhancers(applyMiddleware(penderMiddleware()))
+)
+
+if (rootReducer.hot) {
+	rootReducer.hot.accept("./reducers", () => {
+		const nextRootReducer = require("./reducers").default
+		store.replaceReducer(nextRootReducer)
+	})
+}
+
+export default store
