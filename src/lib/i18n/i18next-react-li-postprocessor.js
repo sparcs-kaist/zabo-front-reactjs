cb919421532ac3e9467de24b4d8655791887da73
jin <jin@coinone.co.id>
2019-05-01 14:58:59 +0900

    feat(i18n): Add i18n libraries and post processors
    i18n custom post processors
    	- li post processor
    	- react post processor

---
+++ src/lib/i18n/i18next-react-li-postprocessor.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { splitReg, render } from "./i18next-react-react-postprocessor"
+
+class Li {
+	type = "postProcessor"
+	name = "li"
+	process = (value, key, options, translator) => {
+		/* return manipulated value */
+		return value.split("\n").map((item, index) => (
+			<li key={index} className={options.className}>
+				{render(item.split(splitReg), options, "1")}
+			</li>
+		))
+	}
+}
+
+export default Li
