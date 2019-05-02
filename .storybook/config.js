f167753f94a913c6f48e54845fc84fb9424e782e
jin <jin@coinone.co.id>
2019-05-02 16:40:14 +0900

    fix(storybook): Add providers

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,32 +1,32 @@
-import { addDecorator, configure, addParameters } from '@storybook/react'
+import { addDecorator, configure } from '@storybook/react'
 import { withNotes } from '@storybook/addon-notes'
 import '@storybook/addon-console'
-import { withInfo } from "@storybook/addon-info"
-import { withKnobs } from '@storybook/addon-knobs';
+//import { withInfo } from "@storybook/addon-info"
+import { withKnobs } from '@storybook/addon-knobs'
 import { withConsole } from '@storybook/addon-console'
 
 import requireContext from 'require-context.macro'
 
 import React from "react"
 
-// import { Provider } from "react-redux"
-// import { BrowserRouter as Router } from "react-router-dom"
-// import { ThemeProvider } from "styled-components"
+import { Provider } from "react-redux"
+import { BrowserRouter as Router } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
 
-// import store from "../src/store"
-// import theme from "../src/theme"
+import store from "../src/store"
+import theme from "../src/theme"
 
-// import '../src/index.scss'
+import '../src/index.scss'
 
-// const Decorator = (storyFn) => (
-// 	<Provider store={store}>
-// 		<Router>
-// 			<ThemeProvider theme={theme}>
-// 				{storyFn()}
-// 			</ThemeProvider>
-// 		</Router>
-// 	</Provider>
-// )
+const Decorator = (storyFn) => (
+	<Provider store={store}>
+		<Router>
+			<ThemeProvider theme={theme}>
+				{storyFn()}
+			</ThemeProvider>
+		</Router>
+	</Provider>
+)
 
 // addDecorator(Decorator)
 addDecorator(withNotes)
@@ -34,9 +34,9 @@ addDecorator((storyFn, context) => withConsole()(storyFn)(context))
 //addDecorator(withInfo)
 addDecorator(withKnobs)
 
-addParameters({
-	chromatic: { viewports: [1320, 992, 768, 576] },
-})
+//addParameters({
+//	chromatic: { viewports: [1320, 992, 768, 576] },
+//})
 
 const req = requireContext('../src/components', true, /\.stories\.js$/)
 
