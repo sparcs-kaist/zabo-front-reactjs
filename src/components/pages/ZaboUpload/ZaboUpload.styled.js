9b3fc5dec12a6d138b6daf3c8eaa3e7ef839fa5b
Bhang Hyoung Jo <bhanghj3094@gmail.com>
2019-05-30 00:16:56 +0900

    ZaboUpload page material-ui added

--- src/components/pages/ZaboUpload/ZaboUpload.styled.js
+++ src/components/pages/ZaboUpload/ZaboUpload.styled.js
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 
 const ZaboUploadWrapper = styled.div`
+  /* ====== body ====== */
   margin: 0;
   padding: 0;
 
@@ -8,10 +9,11 @@ const ZaboUploadWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  
   width: 100%;
-  height: 100%;
   background-color: #efefef;
   
+  /* ====== Upload Section ====== */
   #uploadSection {
     display: flex;
     flex-direction: row;
@@ -20,31 +22,38 @@ const ZaboUploadWrapper = styled.div`
   
     width: 1024px;
     height: 650px;
-    padding: 30px;
+    padding: 40px;
     border-radius: 10px;
+    background-color: white;
   }
   #imageUpload {
-    width: 400px;
-    height: 590px;
+    margin: 20px;
+    width: 360px;
+    height: 550px;
     
-    border: 1px solid gray;
+    border: 1px dotted black;
+    border-radius: 10px;
   }
   #informationUpload {
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
+    justify-content: space-around;
     align-items: center;
     
-    width: 500px;
-    height: 590px;
-    
-    border: 1px solid gray;
+    margin: 20px;
+    width: 460px;
+    height: 550px;
   }
   
-  
+  /* ===== Submit Section ====== */
   #submitSection {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    
     width: 100%;
   }
 `;
 
-export default ZaboUploadWrapper
+export default ZaboUploadWrapper;
