5fc08a78d9add2b76243de805e3db4b003cd9f26
Bhang Hyoung Jo <bhanghj3094@gmail.com>
2019-05-29 22:55:02 +0900

    ZaboUpload page layout2

--- src/components/pages/ZaboUpload/ZaboUpload.styled.js
+++ src/components/pages/ZaboUpload/ZaboUpload.styled.js
@@ -21,14 +21,14 @@ const ZaboUploadWrapper = styled.div`
     width: 100%;
     height: 100%;
     background-color: #efefef;
-    
-    border: 1px solid black;
   }
   
   /* ===== Upload Section ===== */
   #uploadSection {
     display: flex;
     flex-direction: row;
+    justify-content: center;
+    align-items: center;
   
     width: 1024px;
     height: 650px;
