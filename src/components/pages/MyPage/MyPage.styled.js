13d54f2c72db25fa14eeeeda9a57c17f9ccfa396
parang <appdler96@kaist.ac.kr>
2019-05-30 21:07:18 +0900

    mypage created by parang

---
+++ src/components/pages/MyPage/MyPage.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+const MyPageWrapper = styled.div`
+	margin: 0;
+	padding: 0;
+	
+	/* ===== Your Code Here ===== */
+	font-size: 100px;
+	font-color: red;
+`
+
+export default MyPageWrapper
