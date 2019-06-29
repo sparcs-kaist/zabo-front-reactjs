42ac283da95475c3e233db403635e21bf429e9aa
Bhang Hyoung Jo <bhanghj3094@gmail.com>
2019-06-29 22:10:54 +0900

    ZaboUpload layout

--- src/components/pages/ZaboUpload/ZaboUpload.js
+++ src/components/pages/ZaboUpload/ZaboUpload.js
@@ -3,10 +3,10 @@ import PropTypes from "prop-types"
 
 import ZaboUploadWrapper from "./ZaboUpload.styled"
 import Button from '@material-ui/core/Button';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import TextField from '@material-ui/core/TextField';
+import InputBase from '@material-ui/core/InputBase';
 import DateFnsUtils from '@date-io/date-fns';
+import chevron from "../../../static/images/chevron_left.svg";
+import add from "../../../static/images/add.svg";
 import {
 	MuiPickersUtilsProvider,
 	KeyboardDatePicker
@@ -15,7 +15,6 @@ import {
 class ZaboUpload extends PureComponent {
 	constructor(props) {
 		super(props);
-		this.uploadImages = React.createRef();
 	}
 
 	state = {
@@ -34,69 +33,86 @@ class ZaboUpload extends PureComponent {
 		console.log(e.target);
 	};
 
+	_onTagClick = (e) => {
+		console.log(e.target.textContent.slice(1));
+
+	};
+
 	render() {
 		const state = this.state;
 
 		return (
 			<ZaboUploadWrapper>
+				<div id="topline" />
+				<div id="header">
+					<img src={chevron} alt="chevron_left" />
+					<div id="upload_your_poster">
+						Upload Your Poster
+					</div>
+				</div>
 				<form onSubmit={this._handleSubmit}>
-					<div id="uploadSection">
-						<div id="imageUpload">
-							<input
-								accept="image/*"
-								id="imageUploadInput"
-								multiple
-								type="file"
-								ref={this.uploadImages}
-							/>
-							<div id="imageUploadText">
-								<CloudUploadIcon />
-								Upload Image Here
+					<div id="inputs">
+						<section id="zabo">
+							<div className="label">
+								Poster *
 							</div>
-						</div>
-						<div id="informationUpload">
-							<div id="title">
-								<TextField
+							<div id="zaboContainer" className="container">
+								<img src={add} alt="add zabo" />
+							</div>
+						</section>
+						<div id="zaboInfo">
+							<section id="title">
+								<div className="label">
+									Title *
+								</div>
+								<InputBase
 									required
-									autoFocus={true}
-									inputStyle={{ fontSize: '200px' }}
-									label="Title of this Zabo"
-									margin="normal"
-									fullWidth={true}
-								/>
-							</div>
-							<div id="uploader">
-								<AccountCircle />
-								SPARCS
-							</div>
-							<div id="description">
-								<TextField
+									className="container"
+									placeholder="Please type your poster title"
+									multiline />
+							</section>
+							<section id="explanation">
+								<div className="label">
+									Explanation *
+								</div>
+								<InputBase
 									required
-									id="descriptionTextField"
-									label="Description"
+									className="container"
+									placeholder="Please type your poster explanation"
 									multiline
 									rows="12"
-									margin="normal"
-									fullWidth={true}
-								/>
-							</div>
-							<div id="expiration">
-								<MuiPickersUtilsProvider utils={DateFnsUtils}>
-									<KeyboardDatePicker
-										required
-										margin="normal"
-										label="Expiration Date"
-										value={state.selectedDate}
-										onChange={this._handleDateChange}
-									/>
-								</MuiPickersUtilsProvider>
-							</div>
+									fullWidth={true} />
+							</section>
+							<section id="expiration">
+								<div className="label">
+									Expiration Date *
+								</div>
+								<div className="container">
+									<MuiPickersUtilsProvider utils={DateFnsUtils}>
+										<KeyboardDatePicker
+											required
+											value={state.selectedDate}
+											onChange={this._handleDateChange}
+											fullWidth={true} />
+									</MuiPickersUtilsProvider>
+								</div>
+							</section>
+							<section id="keywords">
+								<div className="label">
+									Keyword
+								</div>
+								<div id="tags">
+									<div onClick={this._onTagClick} className="tag">#Advertisement</div>
+									<div onClick={this._onTagClick} className="tag">#Club</div>
+									<div onClick={this._onTagClick} className="tag">#Event</div>
+									<div onClick={this._onTagClick} className="tag">#Recruiting</div>
+									<div onClick={this._onTagClick} className="tag">#Student Council</div>
+								</div>
+							</section>
 						</div>
 					</div>
-					<div id="submitSection">
-						<Button type="submit" variant="contained" size="large" color="secondary">
-							제출하기
-						</Button>
+					<div id="submit">
+						<button type="submit">Sign In</button>
 					</div>
 				</form>
 			</ZaboUploadWrapper>
