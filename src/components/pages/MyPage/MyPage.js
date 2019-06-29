f06eafdb9792e4b96f00ac782f883e375bf654da
Hou <tkdgh97531@kaist.ac.kr>
2019-06-30 06:41:06 +0900

    [Mypage] Make basic templates for MyPage

--- src/components/pages/MyPage/MyPage.js
+++ src/components/pages/MyPage/MyPage.js
@@ -1,13 +1,64 @@
 import React, { PureComponent } from "react"
 import PropTypes from "prop-types"
 
+import SavedPosters from "templates/SavedPosters";
 import MyPageWrapper from "./MyPage.styled"
 
 class MyPage extends PureComponent {
+
+  constructor(props) {
+    super();
+
+    /* 그룹 || 뒤는 추후에 undefined나 null로 설정해둘 예정 */
+    this.state = {
+      username : props.name || 'No Name',
+      group : props.group || 'No group',
+      clicked : 0,
+    }
+  }
+
+  onClick = (e) => {
+    e.preventDefault();
+    const { clicked } = this.state;
+      if ( !clicked ) {
+        this.setState({ clicked : 180});
+      } else {
+        this.setState({clicked : 0});
+      }
+  };
+
+
+
 	render() {
+    const { clicked, group } = this.state;
+
+    let imgRotate = {
+      transform: `rotate(${clicked}deg)`,
+      transition: `all 0.3s ease-out`
+    };
+
+    const groupNames = (
+      /* if not  selected
+      group -> maps. List contents!
+      if selected
+      groupNames -> selected group
+      */
+      <div></div>
+    );
+
 		return (
 			<MyPageWrapper className="animated fadeIn">
-				MyPage Hello~
+        <div className="page-title">My Page </div>
+        <div className="user-name">{this.state.username}</div>
+        { group &&
+          <div className="group-selector" onClick={this.onClick}>
+            <div className="selector"> select </div>
+            {groupNames}
+            <img src={require("static/images/baseline-expand_more-24px.svg")}
+              style={imgRotate}/>
+          </div>
+        }
+        <SavedPosters />
 			</MyPageWrapper>
 		)
 	}
---
+++ src/components/templates/SavedPosters/SavedPosters.stories.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { storiesOf } from "@storybook/react"
+
+import SavedPosters from "./SavedPosters"
+
+storiesOf("templates/SavedPosters", module).add(
+	"Default",
+	() => (
+		<SavedPosters />
+	), {
+		notes: ""
+	}
+)
