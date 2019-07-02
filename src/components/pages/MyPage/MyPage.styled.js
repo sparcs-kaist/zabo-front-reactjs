68dd71c22294933ad93ac1b560f6b801c3323ef1
Hou <tkdgh97531@kaist.ac.kr>
2019-07-03 00:43:26 +0900

    [Mypage] Make Fake protected Route

--- src/components/pages/MyPage/MyPage.styled.js
+++ src/components/pages/MyPage/MyPage.styled.js
@@ -16,32 +16,39 @@ const MyPageWrapper = styled.div`
 	}
 	
 	.user-name {
-	  font-size: 24px;
+    font-size: 24px;
     margin-top: 30px;
     font-family: NanumSquare;
     font-weight: bold;
     color: #143441;
-
 	}
 	
 	.group-selector {
 	  font-size: 16px;
 	  font-weight: bold;
 	  color: #8f8f8f;
-    display: flex;
-    flex-flow: row;
-    align-items: center;
-    width: fit-content;
-    cursor : pointer;
+	  display: flex;
+	  flex-flow: row;
+	  align-items: center;
+	  width: fit-content;
+	  cursor : pointer;
 	  :hover {
 	    
   	}
 	}
 	
 	.selector {
-    margin-left : 2px;
-    font-family: NanumSquare;
-    width: 70px;
+	  margin-left : 2px;
+	  font-family: NanumSquare;
+	  width: 70px;
+	}
+	
+	.group-dropdown {
+	
+	}
+	
+	.group-name {
+	
 	}
 `;
 
