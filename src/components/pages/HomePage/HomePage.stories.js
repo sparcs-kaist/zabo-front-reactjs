c0689eab7d5758c31d0518dceedbe966250969c9
jungdj <jung_dj@kaist.ac.kr>
2019-07-07 01:30:21 +0900

    hotfix(stories): Update all stories to import react component from index.js

--- src/components/pages/HomePage/HomePage.stories.js
+++ src/components/pages/HomePage/HomePage.stories.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { storiesOf } from "@storybook/react"
 
-import HomePage from "./HomePage"
+import HomePage from "./index"
 
 storiesOf("pages/HomePage", module).add(
 	"Default",
--- src/components/templates/SearchBar/SearchBar.stories.js
+++ src/components/templates/SearchBar/SearchBar.stories.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { storiesOf } from "@storybook/react"
 
-import SearchBar from "./SearchBar"
+import SearchBar from "./index"
 
 storiesOf("templates/SearchBar", module).add(
 	"Default",
