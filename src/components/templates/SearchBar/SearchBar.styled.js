9c91e7e260ad2dcec197f44dfa17e4094fefff58
parang <appdler96@kaist.ac.kr>
2019-07-12 00:46:07 +0900

    feat(searchbar) : Apply design

--- src/components/templates/SearchBar/SearchBar.styled.js
+++ src/components/templates/SearchBar/SearchBar.styled.js
@@ -7,7 +7,7 @@ display: flex;
 flex-direction: column;
 
 .search {
-  height: 50px;
+  height: 40px;
   position: relative;
   display: flex;
   flex-direction: row;
@@ -17,9 +17,9 @@ flex-direction: column;
 
 .search-Bar {
     width: 100%;
-    border-radius: 10px;
+    border-radius: 4px;
     outline: none;
-    background-color: #efefef;
+    background-color: #f4f4f4;
 }
 
 .search-input {
@@ -28,17 +28,17 @@ flex-direction: column;
     outline: none;
     border: none;
     margin-left: 10px;
-    background-color: #efefef;
-    font-size : 20px;
+    background-color: #f4f4f4;
+    font-size : 16px;
 } 
 .search-input::placeholder {
-  font-size : 20px;
+  font-size : 16px;
 }
 
 .search-icon {
   position: absolute;
-  right: 20px;
-  height: 20px;
+  right: 16px;
+  height: 16px;
   display: block;
   align-self : center;
 } 
@@ -60,6 +60,30 @@ flex-direction: column;
   &.hide {
     /* visibility: hidden; */
     max-height: 0;
+  }
+  h3 {
+    color: #8f8f8f;
+    font-size: 12px;
+    font-weight: lighter;
+    margin-bottom: 0px;
+  }
+  ul {
+    padding: 0px;
+  }
+  li {
+    padding : 10px 0px 10px 0px;
+    list-style: none;
+    /* margin: 10px; */
+    border-bottom: 1px solid #efefef;
+    font-size: 16px;
+    color: #143441;
+    :last-child {
+      border-bottom: 0px;
+      padding-bottom: 15px;
+    }
+    :first-child {
+      padding-top: 0px;
+    }
   }
 }
 `
