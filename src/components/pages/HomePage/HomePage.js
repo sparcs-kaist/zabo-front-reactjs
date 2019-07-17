657048bbf720a1e61136b049e3fa3a58c76ae5b4
Bhang Hyoung Jo <bhanghj3094@gmail.com>
2019-07-18 05:23:07 +0900

    feat(zabo): Layout for Zabo

--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -2,58 +2,90 @@ import React, { PureComponent } from "react"
 import { Link } from "react-router-dom"  // do not refresh, but render on Link clicked
 import PropTypes from "prop-types"
 
-import HomePageWrapper, { Header } from "./HomePage.styled";
-import SearchBar from 'templates/SearchBar';
-import SVG from "../../atoms/SVG";
+import HomePageWrapper, { Header, ZaboList, Zabo } from "./HomePage.styled"
+import SearchBar from 'templates/SearchBar'
+import SVG from "../../atoms/SVG"
 
 class HomePage extends PureComponent {
-	constructor (props) {
-		super(props)
-	}
-
 	state = {
 		searchFocused: false,
-	};
+	}
 
 	_onSearchFocusBlur = (e) =>
 		this.setState(prevState => {
 			return {
-				searchFocused: !prevState.searchFocused
+				searchFocused: !prevState.searchFocused,
 			}
-		});
+		})
+
+	// @media query 로 몇줄 있어야 하는지 계산.
+	// 자보 요청 보내서 response => 순서대로 가장 길이 짧은 줄에 넣기.
+	//
 
 	render() {
-		const { searchFocused } = this.state;
+		const { searchFocused } = this.state
 
 		return (
 			<HomePageWrapper className="animated fadeIn">
 				<div className="container">
 					<Header>
-						<div className={`blur animated fadeIn ${searchFocused ? "show" : ""}`} />
+						<div className={`blur animated fadeIn ${searchFocused ? "show" : ""}`}/>
 						<Header.Search>
 							<SearchBar
 								onFocus={this._onSearchFocusBlur}
 								onBlur={this._onSearchFocusBlur}
-								isOpen={searchFocused} />
+								isOpen={searchFocused}/>
 						</Header.Search>
-						{ searchFocused ||
+						{searchFocused ||
 						<Link to="/zabo/upload">
 							<Header.AddButton>
-								<SVG icon={'plus'} color="white" size="lg" />
+								<SVG icon={'plus'} color="white" size="lg"/>
 							</Header.AddButton>
 						</Link>
 						}
 					</Header>
+					<ZaboList>
+						<Zabo>
+							<Zabo.Poster>
+								<img
+									width="250px"
+									src="https://about.canva.com/wp-content/uploads/sites/3/2015/01/concert_poster-tb-220x0.png"
+								 />
+							</Zabo.Poster>
+							<Zabo.Writings>
+								<Zabo.Writings.Title>
+									Jazz Concertia
+								</Zabo.Writings.Title>
+								<Zabo.Writings.Author>
+									SPARCS
+								</Zabo.Writings.Author>
+							</Zabo.Writings>
+						</Zabo>
+						<Zabo>
+							<Zabo.Poster>
+								<img
+									width="250px"
+									src="https://piktochart.com/wp-content/uploads/2018/01/poster-conference.jpg"
+								 />
+							</Zabo.Poster>
+							<Zabo.Writings>
+								<Zabo.Writings.Title>
+									Annual Business Conference
+								</Zabo.Writings.Title>
+								<Zabo.Writings.Author>
+									SPARCS2
+								</Zabo.Writings.Author>
+							</Zabo.Writings>
+						</Zabo>
+					</ZaboList>
 				</div>
 			</HomePageWrapper>
 		)
 	}
 }
 
-HomePage.propTypes = {
-};
+HomePage.propTypes = {}
 
-HomePage.defaultProps = {
-};
+HomePage.defaultProps = {}
 
 export default HomePage
--- src/components/pages/HomePage/HomePage.styled.js
+++ src/components/pages/HomePage/HomePage.styled.js
@@ -1,12 +1,24 @@
 import styled from "styled-components"
 
+/* ============ Wrapper ============ */
 const HomePageWrapper = styled.div`
+	@font-face {
+    font-family: 'NanumSquareRegular';
+    src: url(../../../lib/fonts/NanumSquareRegular.ttf) format('truetype');
+  }
+	@font-face {
+    font-family: 'NanumSquareBold';
+    src: url(../../../lib/fonts/NanumSquareBold.ttf) format('truetype');
+  }
+	
 	transition: 0.4s;
 	animation-duration: 0.3s;
-`;
+`
 
+export default HomePageWrapper
+
+/* ============ Header ============ */
 export const Header = styled.div`
-	min-height: 50px;
 	display: flex;
 	align-items: center;
 	
@@ -24,19 +36,42 @@ export const Header = styled.div`
 			background-color: rgba(255, 255, 255);
 		}
 	}
-`;
-
+`
 Header.Search = styled.div`
 	flex: 1;
 	margin-right: 12px;
 	transition: 1s;
 	z-index: 2;
-`;
-
+`
 Header.AddButton = styled.button`
 	width: 60px;
 	height: 40px;
 	background-color: rgb(27, 50, 65);
-`;
+`
 
-export default HomePageWrapper;
+/* ============ ZaboList ============ */
+export const ZaboList = styled.div`
+	margin-top: 20px;
+`
+
+/* ============ Zabo ============ */
+export const Zabo = styled.div`
+	width: 250px;
+	//height: auto;
+`
+Zabo.Poster = styled.div`
+	box-shadow: 0px 3px 6px #A9A9A9;
+`
+Zabo.Writings = styled.div`
+	padding: 15px 10px 20px 10px;
+	font-color: #143441;
+`
+Zabo.Writings.Title = styled.div`
+	font-family: "NanumSquareBold";
+ 	font-size: 14px;
+`
+Zabo.Writings.Author = styled.div`
+	margin-top: 5px;
+	font-family: "NanumSquareRegular";
+	font-size: 10px;
+`
