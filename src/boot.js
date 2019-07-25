f7189d78415f97b6f082f50ab5735196f79e81a4
jungdj <jung_dj@kaist.ac.kr>
2019-07-26 00:03:46 +0900

    feat(pwa): PWA install prompt on top

--- src/boot.js
+++ src/boot.js
@@ -4,6 +4,22 @@ import { bindActionCreators } from "redux"
 import storage from "lib/storage"
 import axios from "lib/axios"
 
+
+const pwaInstallPromptListener = () => {
+	window.addEventListener('beforeinstallprompt', (e) => {
+		// Prevent Chrome 67 and earlier from automatically showing the prompt
+		e.preventDefault();
+		// Stash the event so it can be triggered later.
+		window.deferredPrompt = e;
+		// Update UI notify the user they can add to home screen
+		window.pwaPromptActive = true
+		document.body.classList.add('pwa-prompt-active')
+		if (window.onPWAPromptActive) {
+			window.onPWAPromptActive()
+		}
+	})
+}
+
 export default () => {
 	(function() {
 		var throttle = function(type, name, obj) {
@@ -26,6 +42,8 @@ export default () => {
 		throttle("resize", "optimizedResize")
 		throttle("scroll", "optimizedScroll")
 	})()
+
+	pwaInstallPromptListener()
 
 	const AuthActions = bindActionCreators(authActions, store.dispatch)
 
