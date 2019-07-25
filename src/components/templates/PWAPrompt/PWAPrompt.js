64f18f9e191332df209415fd9bb30ca1a3197cfe
jungdj <jung_dj@kaist.ac.kr>
2019-07-26 00:49:05 +0900

    feat(pwa): Deferred prompt trigger

--- src/components/templates/PWAPrompt/PWAPrompt.js
+++ src/components/templates/PWAPrompt/PWAPrompt.js
@@ -3,14 +3,22 @@ import PropTypes from "prop-types"
 
 import PWAPromptWrapper from "./PWAPrompt.styled"
 
+import appIcon from "static/logo/sparcs.svg"
+
+const handleScroll = e => {
+	if (window.scrollY < 10) {
+		document.body.classList.add('pwa-prompt-active')
+	} else {
+		document.body.classList.remove('pwa-prompt-active')
+	}
+}
+
 const addListener = () => {
-	window.addEventListener("optimizedScroll", a => {
-		if (window.scrollY < 10) {
-			document.body.classList.add('pwa-prompt-active')
-		} else {
-			document.body.classList.remove('pwa-prompt-active')
-		}
-	})
+	window.addEventListener("optimizedScroll", handleScroll)
+}
+
+const deletelistener = () => {
+	window.removeEventListener('optimizedScroll', handleScroll)
 }
 
 class PWAPrompt extends PureComponent {
@@ -24,10 +32,34 @@ class PWAPrompt extends PureComponent {
 		}
 	}
 
+	handleOpenClick = () => {
+		deletelistener()
+		document.body.classList.remove('pwa-prompt-active')
+
+		window.deferredPrompt.prompt();
+		// Wait for the user to respond to the prompt
+		window.deferredPrompt.userChoice
+			.then((choiceResult) => {
+				if (choiceResult.outcome === 'accepted') {
+					console.log('User accepted the A2HS prompt');
+				} else {
+					console.log('User dismissed the A2HS prompt');
+				}
+				window.deferredPrompt = null;
+			});
+	}
+
 	render() {
 		return (
 			<PWAPromptWrapper className="pwa-prompt">
-				PWAPrompt
+				<div className="container">
+						<img src={appIcon} alt="" className="logo"/>
+					<div className="texts">
+						<div className="title">ZABO (자보) : 모든 포스터를 한 곳에서 모아보세요 asds</div>
+						<div className="desc">ZABO 어플리케이션 설치하기</div>
+					</div>
+					<button onClick={this.handleOpenClick}>설치</button>
+				</div>
 			</PWAPromptWrapper>
 		)
 	}
