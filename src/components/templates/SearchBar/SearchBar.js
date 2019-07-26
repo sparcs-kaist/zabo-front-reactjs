80e5b22ff57e35ec94151adcd907923d88703efd
Bhang Hyoung Jo <bhanghj3094@gmail.com>
2019-07-27 04:19:50 +0900

    feat(search): debounce api

--- src/components/templates/SearchBar/SearchBar.js
+++ src/components/templates/SearchBar/SearchBar.js
@@ -1,21 +1,55 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import PropTypes from "prop-types"
+import AwesomeDebouncePromise from 'awesome-debounce-promise'
+import axios from 'lib/axios'
 
 import SearchBarWrapper from "./SearchBar.styled"
+
 import searchIcon from 'static/images/search-icon-navy.png'
+
+/* ==== search bar debounce ==== */
+const searchAPI = text => {
+	if (!text) return Promise.resolve({ data: { zabos: [], groups: [], categories: [] } })
+	return axios.get('/search?query=' + encodeURIComponent(text))
+}
+const searchAPIDebounced = AwesomeDebouncePromise(searchAPI, 1000)
 
 class SearchBar extends React.Component {
 	state = {
-		search: '',
+		search: "",
+		zaboSearch: [],
+		uploaderSearch: [],
+		keywordSearch: [],
+		searchResults: {},
 	}
 
-	_handleChange = (e) => {
-		this.setState({ search: e.target.value })
+	_updateResults = (data) => {
+		const { zabos, groups, categories } = data
+		this.setState({
+			zaboSearch: zabos,
+			uploaderSearch: groups,
+			keywordSearch: categories,
+		})
 	}
 
-	///search?query=
+	_handleChange = async (e) => {
+		const { value: query } = e.target
+		this.setState({ search: query, }) // keyboard inputs
+		if (this.state.searchResults[query]) // load from caching
+			this._updateResults(this.state.searchResults[query])
+		const res = await searchAPIDebounced(query)
+		this._updateResults(res.data)
+		this.setState(prevState => ({
+			searchResults: {
+				...prevState.searchResults,
+				[query]: res.data
+			},
+		})) // caching
+	}
 
 	render() {
+		const { zaboSearch, uploaderSearch, keywordSearch } = this.state
 		const { isOpen, ...props } = this.props
 
 		return (
@@ -27,23 +61,47 @@ class SearchBar extends React.Component {
 					<img className="search-icon" src={searchIcon}/>
 				</div>
 				<div className={`search-result ${isOpen ? 'show' : 'hide'}`}>
-					<h3>Title</h3>
+					<h3>Zabo</h3>
 					<ul>
-						<li>SPARCS 2018 Spring Recruiting</li>
-						<li>SPARCS 2019 Spring Recruiting</li>
-						<li>SPARCS 2020 Spring Recruiting</li>
+						{
+							zaboSearch.map( zabo =>
+								<li>
+									<Link to="/">
+										{ zabo.title }
+									</Link>
+								</li>
+							)
+						}
+						{
+							zaboSearch.length === 0 && <li>No Results</li>
+						}
 					</ul>
 					<h3>Uploader</h3>
 					<ul>
-						<li>SPARCS</li>
+						{
+							uploaderSearch.map( uploader =>
+								<li>
+									<Link to="/">
+										{ uploader.name }
+									</Link>
+								</li>
+							)
+						}
+						{
+							uploaderSearch.length === 0 && <li>No Results</li>
+						}
 					</ul>
 					<h3>Keyword</h3>
 					<ul className="keyword-result">
-						<li>#Advertisment</li>
-						<li>#Club</li>
-						<li>#Event</li>
-						<li>#Recruiting</li>
-						<li>#Student Concil</li>
+						{
+							keywordSearch.map( keyword =>
+								<li>
+									<Link to="/">
+										{ keyword }
+									</Link>
+								</li>
+							)
+						}
 					</ul>
 				</div>
 			</SearchBarWrapper>
