a4e7107cd38c4c2a5dab29b16ed3c41e28773789
Choiyounseo <dbstj72@kaist.ac.kr>
2020-01-17 06:14:50 +0900

    feat(profile): Add groupProfile page style design

--- src/components/pages/ProfilePage/ProfileGroupPage.js
+++ src/components/pages/ProfilePage/ProfileGroupPage.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Page, Intro } from './Profile.styled';
+import Header from '../../templates/Header';
 
 import { GroupType } from '../../../lib/propTypes';
 
+import defaultProfile from '../../../static/images/defaultProfile.png';
+import groupDefaultProfile from '../../../static/images/groupDefaultProfile.png';
+
 const GroupProfile = ({ profile }) => {
-  const { name } = profile;
-  return <h1>Group {name}</h1>;
+  const {
+    name, profilePhoto, backgroundPhoto,
+  } = profile;
+  return (
+    <Page>
+      <Header rightGroup={<Header.AuthDropdown />} />
+      <Page.Header>
+        <Page.Header.BackPhoto>
+          {
+            backgroundPhoto
+              ? <div style={{ backgroundImage: `url(${backgroundPhoto})` }}> </div>
+              : <div>Background placeholder</div>
+          }
+        </Page.Header.BackPhoto>
+        <Page.Header.ProfilePhoto>
+          {
+            profilePhoto
+              ? <img src={profilePhoto} alt="profile photo" />
+              : <img src={defaultProfile} alt="default profile img" />
+          }
+        </Page.Header.ProfilePhoto>
+        <Page.Header.UserInfo>
+          {name}
+          <button>업로드</button>
+        </Page.Header.UserInfo>
+      </Page.Header>
+
+      <Intro>
+        <h1>소개</h1>
+        <div>Hi, we are sparcs :)</div>
+      </Intro>
+    </Page>
+  );
 };
 
 GroupProfile.propTypes = {
