ddcbf18a3f9a56a727ba4ae7fbc8115d3fcdb311
jungdj <jung_dj@kaist.ac.kr>
2020-01-17 03:53:16 +0900

    refactor(selector): Remove redundant selectors

--- src/lib/utils/selector.js
+++ src/lib/utils/selector.js
@@ -1,24 +1,12 @@
-/* eslint-disable */
-
-/* Selectors */
 import { createSelector } from 'reselect';
 
-// import { mediaSizes } from "lib/variables"
-
 const decodedTokenSelector = state => state.getIn (['auth', 'jwt']);
-// const emailSelector = state => state.getIn(["auth", "info", "email"])
-const userInfoSelect = state => state.getIn (['user', 'info']);
-const locationSelector = state => state.getIn (['router', 'location']);
-// const deviceSelector = state => state.getIn(["base", "window", "width"])
-// const ratesSelector = state => state.getIn(["market", "rates"])
-
 const isDecodedTokenAlive = decoded => {
   if (decoded && decoded.get ('exp')) {
     return 1000 * decoded.get ('exp') - new Date ().getTime () >= 5000;
   }
   return false;
 };
-
 const decodedTokenLifetime = decoded => {
   if (decoded && decoded.get ('exp')) {
     const remain = decoded.get ('exp') * 1000 - Date.now ();
@@ -28,21 +16,4 @@ const decodedTokenLifetime = decoded => {
 };
 
 export const selectAuthenticated = createSelector (decodedTokenSelector, isDecodedTokenAlive);
-
 export const tokenTimeLeft = createSelector (decodedTokenSelector, decodedTokenLifetime);
-
-export const getUserState = createSelector (userInfoSelect, info => info.get ('state'));
-
-// export const isTwoFactorNeeded = createSelector(
-//  decodedTokenSelector,
-//  access => isDecodedTokenAlive(access) && !access.get("authorized")
-// )
-
-export const getCurrencyPair = createSelector (locationSelector, location => location.get ('pathname').substring (10));
-
-// export const getDevice = createSelector(deviceSelector, width => {
-//  let device = "mobile"
-//  if (mediaSizes.tablet < width) device = "tablet"
-//  if (mediaSizes.desktop < width) device = "desktop"
-//  return device
-// })
