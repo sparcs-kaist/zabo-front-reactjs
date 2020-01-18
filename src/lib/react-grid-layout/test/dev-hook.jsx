cb3f10f5790e32e3d10454e3e63ac55e4c3a3319
jungdj <jung_dj@kaist.ac.kr>
2020-01-18 23:35:09 +0900

    build(package): React-grid-layout from @sparcs-kaist/

--- src/lib/react-grid-layout/test/dev-hook.jsx
+++
@@ -1,8 +0,0 @@
-import "react-hot-loader";
-import { hot } from "react-hot-loader/root";
-import DevLayout from "./examples/0-showcase.jsx";
-import makeLayout from "./test-hook";
-
-const Layout = makeLayout(DevLayout);
-
-export default hot(Layout);
