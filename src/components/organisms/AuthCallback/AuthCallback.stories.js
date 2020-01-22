d6eeba17f3ed8f6e092694eac6c36b45d56eb625
jungdj <jung_dj@kaist.ac.kr>
2020-01-23 06:57:33 +0900

    refactor(cleanup): Remove redundant codes

--- src/components/organisms/AuthCallback/AuthCallback.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import AuthCallback from './index';
-
-storiesOf ('organisms/AuthCallback', module).add ('Default', () => <AuthCallback />, {
-  notes: '',
-});
