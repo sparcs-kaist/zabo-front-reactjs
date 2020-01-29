504ab4dea1cc81db37dc7a9ea8c0d98e27874b5f
jungdj <jung_dj@kaist.ac.kr>
2020-01-29 23:09:01 +0900

    fix(ErrorBoundary): Fix infinite loop with error boundary

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import { Route, Switch } from 'react-router-dom';
-
 import PWAPrompt from 'templates/PWAPrompt';
 import {
   AuthPage,
@@ -18,6 +16,7 @@ import WindowResizeListener from 'containers/WindowResizeListener';
 import ScrollToTop from 'containers/ScrollToTop';
 
 import pToP from 'hoc/paramsToProps';
+import ErrorBoundary from './components/ErrorBoundary';
 
 import AppWrapper from './App.styled';
 
@@ -25,45 +24,25 @@ import { AdminRoute, PrivateRoute, PublicRoute } from './hoc/AuthRoutes';
 
 import 'lib/channel_io';
 
-class App extends React.Component {
-  componentDidCatch (error, errorInfo) {
-    // Catch errors in any components below and re-render with error message
-    if (process.env.NODE_ENV !== 'production') {
-      this.setState ({
-        error,
-        errorInfo: { ...errorInfo, url: window.location.href },
-      });
-    } else {
-      alert ('Something went wrong. Please contact us if this error continues to appear.');
-      // TODO: Crash Report
-      // crashReport({
-      // error: error.toString(),
-      // errorInfo: { ...errorInfo, url: window.location.href },
-      // })
-    }
-    // You can also log error messages to an error reporting service here
-  }
-
-  render () {
-    return (
-      <AppWrapper>
-        <WindowResizeListener />
-        <PWAPrompt />
-        <Route path="/zabo/:route?" component={ScrollToTop} />
-        <Route path="/" exact component={AuthCallback} />
-        <Switch>
-          <Route path="/zabo/upload" component={ZaboUploadPage} />
-          <Route path="/zabo/:zaboId" component={pToP (ZaboPage)} />
-          <PublicRoute path="/auth" component={AuthPage} />
-          <PrivateRoute path="/settings" component={SettingsPage} />
-          <AdminRoute path="/admin" component={AdminPage} />
-          <Route path="/" exact component={HomePage} />
-          <Route path="/:name" component={ProfilePage} />
-          <Route component={NotFound} />
-        </Switch>
-      </AppWrapper>
-    );
-  }
-}
+const App = () => (
+  <AppWrapper>
+    <ErrorBoundary>
+      <WindowResizeListener />
+      <PWAPrompt />
+      <Route path="/zabo/:route?" component={ScrollToTop} />
+      <Route path="/" exact component={AuthCallback} />
+      <Switch>
+        <Route path="/zabo/upload" component={ZaboUploadPage} />
+        <Route path="/zabo/:zaboId" component={pToP (ZaboPage)} />
+        <PublicRoute path="/auth" component={AuthPage} />
+        <PrivateRoute path="/settings" component={SettingsPage} />
+        <AdminRoute path="/admin" component={AdminPage} />
+        <Route path="/" exact component={HomePage} />
+        <Route path="/:name" component={ProfilePage} />
+        <Route component={NotFound} />
+      </Switch>
+    </ErrorBoundary>
+  </AppWrapper>
+);
 
 export default App;
