7d50541f5f1401316fc25d532178245a1c48b6b3
jungdj <jung_dj@kaist.ac.kr>
2020-02-22 05:13:42 +0900

    fix(PWA): Fix lint

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -18,50 +18,11 @@ const isLocalhost = Boolean (
     || window.location.hostname.match (/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/),
 );
 
-export function register (config) {
-  if ((process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) || true) {
-    // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL (process.env.PUBLIC_URL, window.location.href);
-    if (publicUrl.origin !== window.location.origin) {
-      // Our service worker won't work if PUBLIC_URL is on a different origin
-      // from what our page is served on. This might happen if a CDN is used to
-      // serve assets; see https://github.com/facebook/create-react-app/issues/2374
-      return;
-    }
-
-    window.addEventListener ('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
-      if (isLocalhost) {
-        // This is running on localhost. Let's check if a service worker still exists or not.
-        checkValidServiceWorker (swUrl, config);
-
-        // Add some additional logging to localhost, pointing developers to the
-        // service worker/PWA documentation.
-        navigator.serviceWorker.ready.then (() => {
-          console.log (
-            'This web app is being served cache-first by a service '
-              + 'worker. To learn more, visit https://bit.ly/CRA-PWA',
-          );
-        });
-      } else {
-        // Is not localhost. Just register service worker
-        registerValidSW (swUrl, config);
-      }
-    });
-  }
-
-  if ('serviceWorker' in navigator && 'PushManager' in window) {
-    console.log ('Service Worker and Push is supported');
-  } else {
-    console.warn ('Push messaging is not supported');
-  }
-}
-
 function registerValidSW (swUrl, config) {
   navigator.serviceWorker
     .register (swUrl)
     .then (registration => {
+      // eslint-disable-next-line no-param-reassign
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         if (installingWorker == null) {
@@ -75,7 +36,7 @@ function registerValidSW (swUrl, config) {
               // content until all client tabs are closed.
               console.log (
                 'New content is available and will be used when all '
-                  + 'tabs for this page are closed. See https://bit.ly/CRA-PWA.',
+                + 'tabs for this page are closed. See https://bit.ly/CRA-PWA.',
               );
 
               // Execute callback
@@ -126,6 +87,46 @@ function checkValidServiceWorker (swUrl, config) {
     .catch (() => {
       console.log ('No internet connection found. App is running in offline mode.');
     });
+}
+
+export function register (config) {
+  if ((process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) || true) {
+    // The URL constructor is available in all browsers that support SW.
+    const publicUrl = new URL (process.env.PUBLIC_URL, window.location.href);
+    if (publicUrl.origin !== window.location.origin) {
+      // Our service worker won't work if PUBLIC_URL is on a different origin
+      // from what our page is served on. This might happen if a CDN is used to
+      // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+      return;
+    }
+
+    window.addEventListener ('load', () => {
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+
+      if (isLocalhost) {
+        // This is running on localhost. Let's check if a service worker still exists or not.
+        checkValidServiceWorker (swUrl, config);
+
+        // Add some additional logging to localhost, pointing developers to the
+        // service worker/PWA documentation.
+        navigator.serviceWorker.ready.then (() => {
+          console.log (
+            'This web app is being served cache-first by a service '
+              + 'worker. To learn more, visit https://bit.ly/CRA-PWA',
+          );
+        });
+      } else {
+        // Is not localhost. Just register service worker
+        registerValidSW (swUrl, config);
+      }
+    });
+  }
+
+  if ('serviceWorker' in navigator && 'PushManager' in window) {
+    console.log ('Service Worker and Push is supported');
+  } else {
+    console.warn ('Push messaging is not supported');
+  }
 }
 
 export function unregister () {
