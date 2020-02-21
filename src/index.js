c6c139a0f3fb9fe5b033dc783f9c03cacb3d370c
jungdj <jung_dj@kaist.ac.kr>
2020-02-22 05:11:58 +0900

    feat(PWA): Register service worker

--- src/index.js
+++ src/index.js
@@ -11,8 +11,7 @@ import store from 'store';
 
 import App from './App';
 import boot from './boot';
-
-// import * as serviceWorker from "./serviceWorker"
+import * as serviceWorker from './serviceWorker';
 
 boot ();
 
@@ -30,4 +29,4 @@ ReactDOM.render (
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.register()
+serviceWorker.register ();
