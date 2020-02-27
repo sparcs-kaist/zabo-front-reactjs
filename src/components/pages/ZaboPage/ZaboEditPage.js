25f5cde796754af13604c0bc641e6655c2c017b4
jungdj <jung_dj@kaist.ac.kr>
2020-02-28 01:43:29 +0900

    fix(ZaboEdit): Remove schedule on toggle off

--- src/components/pages/ZaboPage/ZaboEditPage.js
+++ src/components/pages/ZaboPage/ZaboEditPage.js
@@ -84,6 +84,7 @@ const ZaboEditPage = ({ zaboId, zabo }) => {
       return;
     }
     const data = { ...state };
+    if (!hasSchedule) delete data.schedules;
     data.category = data.category.filter (t => t.clicked).map (t => t.name).join ('#');
     dispatch (patchZabo ({ zaboId, data }))
       .then (() => {
@@ -111,8 +112,6 @@ const ZaboEditPage = ({ zaboId, zabo }) => {
       setChanged (false);
     }
   }, [state]);
-
-  console.log (state);
 
   return (
     <PageWrapper>
