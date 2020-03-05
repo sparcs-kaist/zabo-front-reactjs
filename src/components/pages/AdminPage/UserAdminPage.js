3479d3901e8a8af25b585bf0d963033f3fa30e8b
jungdj <jung_dj@kaist.ac.kr>
2020-03-05 18:46:27 +0900

    feat(Admin): Admin user table

--- src/components/pages/AdminPage/UserAdminPage.js
+++ src/components/pages/AdminPage/UserAdminPage.js
@@ -5,18 +5,18 @@ import { useSelector } from 'react-redux';
 
 import GridContainer from './components/Grid/GridContainer';
 import UserCard from './UserCard';
+import UserList from './UsersTable';
 
 const UserAdminPage = () => {
   const usersIm = useSelector (state => state.getIn (['admin', 'users']));
   const users = useMemo (() => usersIm.toJS (), [usersIm]);
-
   return (
     <div>
-      <h2>Users</h2>
       <GridContainer>
-        {users.map (user => (
-          <UserCard user={user} />
-        ))}
+        {!!users.length && <UserList users={users} /> }
+        {/* {users.map (user => ( */}
+        {/*  <UserCard user={user} /> */}
+        {/* ))} */}
       </GridContainer>
     </div>
   );
