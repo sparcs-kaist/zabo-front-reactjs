f6d410cea84f4d4550d73e09c8af12bf95c4ce3e
Soonho Kwon <percy3368@gmail.com>
2023-03-03 04:49:16 +0900

    fix(axios): rewrite custom axios instance
    - Rollback to wrapping `axios` instance
    - Add explicit generic typings to match return types from interceptor

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,26 +1,31 @@
-import { Axios } from "axios";
+import axios, { type AxiosInstance, type AxiosRequestConfig as Config } from "axios";
 import { env } from "../env";
 
-class CustomAxios extends Axios {
+class Axios {
+  private static instance: Axios;
+
   private token?: string;
   private interceptor: number | null;
-
-  private static instance: CustomAxios;
+  private session: AxiosInstance;
 
   private constructor() {
-    super({
+    this.session = axios.create({
+      headers: {
+        post: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      },
       timeout: 30000,
       baseURL: `${env.REACT_APP_API_HOST || ""}/api`,
       withCredentials: true,
     });
-    this.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
     this.interceptor = null;
     this.interceptResponse();
   }
 
   public static getInstance() {
     if (!this.instance) {
-      this.instance = new CustomAxios();
+      this.instance = new Axios();
     }
     return this.instance;
   }
@@ -35,13 +40,13 @@ class CustomAxios extends Axios {
     const { token } = this;
 
     if (this.interceptor !== null) {
-      this.interceptors.request.eject(this.interceptor);
+      this.session.interceptors.request.eject(this.interceptor);
       this.interceptor = null;
     }
 
     if (!token) return;
 
-    this.interceptor = this.interceptors.request.use((config) => {
+    this.interceptor = this.session.interceptors.request.use((config) => {
       if (config.url?.[0] === "/") {
         Object.assign(config, {
           headers: {
@@ -56,13 +61,34 @@ class CustomAxios extends Axios {
   }
 
   private interceptResponse() {
-    this.interceptors.response.use(
+    this.session.interceptors.response.use(
       (res) => res.data,
       (error) => {
         throw error?.response?.data || error;
       },
     );
   }
+
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  get = <T, D = any>(url: string, config?: Config<D>) => this.session.get<T, T, D>(url, config);
+  delete = <T, D = any>(url: string, config?: Config<D>) =>
+    this.session.delete<T, T, D>(url, config);
+  head = <T, D = any>(url: string, config?: Config<D>) => this.session.head<T, T, D>(url, config);
+  options = <T, D = any>(url: string, config?: Config<D>) =>
+    this.session.options<T, T, D>(url, config);
+  post = <T, D = any>(url: string, data: D, config?: Config<D>) =>
+    this.session.post<T, T, D>(url, data, config);
+  put = <T, D = any>(url: string, data: D, config?: Config<D>) =>
+    this.session.put<T, T, D>(url, data, config);
+  patch = <T, D = any>(url: string, data: D, config?: Config<D>) =>
+    this.session.patch<T, T, D>(url, data, config);
+  postForm = <T, D = any>(url: string, data: D, config?: Config<D>) =>
+    this.session.post<T, T, D>(url, data, config);
+  putForm = <T, D = any>(url: string, data: D, config?: Config<D>) =>
+    this.session.put<T, T, D>(url, data, config);
+  patchForm = <T, D = any>(url: string, data: D, config?: Config<D>) =>
+    this.session.patch<T, T, D>(url, data, config);
+  /* eslint-enable @typescript-eslint/no-explicit-any */
 }
 
-export default CustomAxios.getInstance();
+export default Axios.getInstance();
