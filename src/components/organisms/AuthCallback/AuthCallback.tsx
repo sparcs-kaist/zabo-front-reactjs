c98d415ae9fb64f6cb5a5745e1e3c1e8c7460a87
Soonho Kwon <percy3368@gmail.com>
2023-04-01 22:34:27 +0900

    feat(typescript): temporary type fixes for `AuthCallback` component

--- src/components/organisms/AuthCallback/AuthCallback.tsx
+++ src/components/organisms/AuthCallback/AuthCallback.tsx
@@ -5,9 +5,17 @@ import type { RouteComponentProps } from "react-router-dom";
 
 import { loginCallback } from "store/reducers/auth";
 import storage from "lib/storage";
+import type { Action } from "redux-actions";
+
+// TODO
+// This is a temporary fix to avoid type errors
+// We should find a way to infer types from redux middlewares
+interface Dispatch {
+  <P>(action: Action<P>): P;
+}
 
 const AuthCallback: React.FC<RouteComponentProps> = ({ location, history }) => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<Dispatch>();
   useEffect(() => {
     const { code, state } = queryString.parse(location.search);
     if (typeof code === "string" && typeof state === "string") {
