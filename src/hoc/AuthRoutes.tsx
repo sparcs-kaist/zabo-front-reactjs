064f29bd157986a52e9d20ba1bb635af14882c28
Soonho Kwon <percy3368@gmail.com>
2023-04-01 23:21:03 +0900

    fix(typescript): fix typescript issues in `PublicRoute` component

--- src/hoc/AuthRoutes.tsx
+++ src/hoc/AuthRoutes.tsx
@@ -1,16 +1,17 @@
 import React from "react";
-import { Redirect, Route, RouteProps } from "react-router-dom";
+import { Redirect, Route, type RouteComponentProps, type RouteProps } from "react-router-dom";
+import { type StaticContext } from "react-router";
 import { useSelector } from "react-redux";
 
 import { isAdminOrPendingSelector, isAuthedSelector } from "lib/utils";
 
 import { alerts } from "../lib/variables";
 
-export const PrivateRoute = ({
+export const PrivateRoute: React.FC<RouteProps> = ({
   component: Component,
   render = () => null,
   ...rest
-}: RouteProps) => {
+}) => {
   const isAuthenticated = useSelector(isAuthedSelector);
   // TODO: Return loading while auth info not fetched
   return (
@@ -25,7 +26,10 @@ export const PrivateRoute = ({
           )
         ) : window.confirm(alerts.login) ? (
           <Redirect
-            to={{ pathname: "/auth/login", state: { referrer: props.location.pathname } }}
+            to={{
+              pathname: "/auth/login",
+              state: { referrer: props.location.pathname },
+            }}
           />
         ) : (
           <Redirect to="/" />
@@ -35,20 +39,32 @@ export const PrivateRoute = ({
   );
 };
 
-export const PublicRoute = ({ component: Component, render = () => null, ...rest }: RouteProps) => {
+export const PublicRoute: React.FC<RouteProps> = ({
+  component: Component,
+  render = () => null,
+  ...rest
+}) => {
   const isAuthenticated = useSelector(isAuthedSelector);
   return (
     <Route
       {...rest}
       render={(props) => {
         if (isAuthenticated) {
-          const { location, history } = props;
-          const { state } = location;
-          // @ts-ignore
-          if (state && state.referrer === "comeback") history.goBack();
-          // @ts-ignore
-          else if (state && state.referrer) history.replace(state.referrer);
-          else history.push("/", { referrer: "public" });
+          const {
+            location: { state },
+            history,
+          } = props as RouteComponentProps<
+            { [key: string]: string | undefined },
+            StaticContext,
+            { referrer?: string }
+          >;
+          if (state && state.referrer === "comeback") {
+            history.goBack();
+          } else if (state && state.referrer) {
+            history.replace(state.referrer);
+          } else {
+            history.push("/", { referrer: "public" });
+          }
           return null;
         }
         if (Component) return <Component {...props} />;
