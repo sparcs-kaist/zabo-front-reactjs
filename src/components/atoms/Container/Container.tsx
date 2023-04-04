fa77bc9d2a1cda2c1604fd7421128b534acd8eac
Soonho Kwon <percy3368@gmail.com>
2023-04-04 11:30:59 +0900

    fix(typescript): add typings to `Container` component

--- src/components/atoms/Container/Container.tsx
+++ src/components/atoms/Container/Container.tsx
@@ -1,19 +1,22 @@
-import PropTypes from "prop-types";
 import styled, { css } from "styled-components";
 
 import { colors } from "lib/theme";
+import { media } from "lib/utils/style";
 
-import { media } from "../../../lib/utils/style";
-
-const Container = styled.div`
+const ContainerComponent = styled.div<{
+  ownStyle?: string | string[];
+  background?: keyof typeof colors;
+}>`
   width: 100%;
   margin: 0 auto;
   padding: 0 24px;
   display: flex;
   overflow: scroll;
+
   &::-webkit-scrollbar {
     width: 0 !important;
   }
+
   -ms-overflow-style: none;
   ${media.tablet(css`
     padding: 0 18px;
@@ -22,19 +25,10 @@ const Container = styled.div`
   background: ${(props) => (props.background ? props.theme[props.background] : "transparent")};
 `;
 
-Container.Pad = styled.div`
-  padding-left: ${(props) => props.width};
+const PadComponent = styled.div<{ width?: number }>`
+  padding-left: ${({ width = "24px" }) => width};
 `;
-Container.Pad.propTypes = {
-  width: PropTypes.number.isRequired,
-};
-Container.Pad.defaultProps = {
-  width: "24px",
-};
 
-Container.propTypes = {
-  ownStyle: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
-  background: PropTypes.oneOf(Object.keys(colors)),
-};
+const Container = Object.assign(ContainerComponent, { Pad: PadComponent });
 
 export default Container;
